3d9a02623b737ff715c2ac966b3ba9069b959515
whiskycheats-a11y <whiskycheats@gmail.com>
2026-02-08 16:07:00 +0530

    9

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react({
+    jsxRuntime: 'classic'
+  })],
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 });
